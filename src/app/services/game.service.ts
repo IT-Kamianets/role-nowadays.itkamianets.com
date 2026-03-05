132fab5cf902f1618cc90f9b6e5df9bb16400ef5
Honchar Denys <crackeraki@gmail.com>
2026-03-05 19:56:47 +0200

    drop sample using API

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,15 +1,29 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { Game } from '../models/game.model';
 
 const BASE = 'https://api.webart.work/api/rnd';
-const OPTIONS = { withCredentials: true };
+const OPTIONS = { headers: { token: '' } };
 
 @Injectable({ providedIn: 'root' })
 export class GameService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.http
+      .post<string>(`${BASE}/token`, {
+        name: 'test',
+      })
+      .subscribe((token) => {
+        OPTIONS.headers.token = token;
+
+        this.http.get<Game[]>(`${BASE}/games`, OPTIONS).subscribe((games) => {
+          console.log(games);
+        });
+
+        this.http.post<Game>(`${BASE}/create`, {}, OPTIONS).subscribe((game) => {});
+      });
+  }
 
   getGames(): Observable<Game[]> {
     return this.http.get<Game[]>(`${BASE}/games`, OPTIONS);
