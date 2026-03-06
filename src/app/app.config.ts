eeb1b8f5aa190ed99f94f61be573066e69c90e3c
Andre123451 <andrijdanilcuk2@gmail.com>
2026-03-06 19:46:54 +0200

    feat: update server options in angular.json and switch to real API in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,7 @@ import { routes } from './app.routes';
 import { GameService } from './services/game.service';
 import { MockGameService } from './services/mock-game.service';
 
-const USE_MOCK = true; // 👈 змінити на false для реального API
+const USE_MOCK = false; // 👈 змінити на false для реального API
 
 export const appConfig: ApplicationConfig = {
   providers: [
